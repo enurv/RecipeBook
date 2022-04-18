1b2ddefa8cb61efb03ee2bde8cdbec71838fee6c
Eda Nur Var <var.eda.nur@gmail.com>
2022-04-18 05:54:20 +0300

    feat: Add recipe edit form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe(
@@ -27,6 +28,8 @@ export class RecipeService {
 
     constructor(private shoppingLisService: ShoppingListService){}
 
+    recipesChanged = new Subject<Recipe[]>();
+
     getRecipes() {
         return this.recipes.slice();
     }
@@ -37,5 +40,20 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]){
         this.shoppingLisService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+    
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+    
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
